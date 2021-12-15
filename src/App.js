757840befc4ef4c359a7fd1171aa49ffe19e93b1
LinhBui88 <87442413+Linhbui88@users.noreply.github.com>
2021-12-14 22:28:39 -0500

    API moduel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Header from './components/header'
 import SearchForm from './components/searchForm';
+import API from'./ultils/API'
 import './App.css';
 
 function App() {
-  const [searchTerm, setSearchTerm]= useState('')
+  const [searchTerm, setSearchTerm]= useState('Abba')
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    getRecommendations()
+  }, [])
+  const getRecommendations = async()=>{
+    setLoading(true)
+    try {
+      const results =await API.getRecommendations(searchTerm)
+      console.log(results)
+    } catch(error) {
+      alert(error)
+    } 
+    setLoading(false)  
+  
+  }
 
   return(
     <>
